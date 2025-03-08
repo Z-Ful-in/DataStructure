35585f13374d6264a706112ea55e18c49138b3bb
浩然 刘 <chi535595@163.com>
2025-03-08 13:58:06 +0800

    排序算法

--- DataStructure/Vector.h
+++ DataStructure/Vector.h
@@ -1,13 +1,32 @@
 #pragma once
-
+#include<iostream>
 #define DEFAULT_CAPACITY 10 // 默认的vector的初始大小
-
+using namespace std;
 template <typename T>
 class Vector{
 protected:
 	int _size; // vector中已装填的元素个数
 	T* _elem; // 指向元素数组的指针
 	int _capacity; // vector的容量
+
+	int getMax(int lo,int hi)//找最值函数
+	{
+		int max = lo;
+		for (int i = lo+1;i <hi;i++)
+		{
+			if (_elem[i] > _elem[max])
+				max = i;
+		}
+		return max;
+	}
+
+	void swap(T& a, T& b)
+	{
+		T temp;
+		temp = a;
+		a = b;
+		b = temp;
+	}
 	
 	void copyFrom(T const* A, int lo, int hi); // 复制数组区间A[lo, hi): 包头不包尾
 	void shrink(); // 空间过大时压缩
@@ -58,5 +77,134 @@ public:
 	void traverse(void(*)(T&)); //通过函数指针遍历
 	template <typename VST> void traverse(VST&); //通过模板遍历
 
+	void print()const
+	{
+		for (int i = 0;i < _size;i++)
+		{
+			cout  << _elem[i]<<" ";
+
+		}
+		cout << endl;
+	}
+
 };
 
+template<typename T>
+void Vector<T>::copyFrom(T const* A, int lo, int hi)
+{
+	_elem = new T[_capacity = 2 * (hi - lo)];
+	_size = 0;
+	while (lo < hi) {
+		_elem[_size++] = A[lo++];
+	}
+}
+
+template<typename T>
+inline void Vector<T>::selectSort(int lo, int hi)
+{
+	for (int k = hi-1;k>lo;k--)
+	{
+		swap(_elem[k],_elem[getMax(lo, k+1)]);
+	}
+}
+template<typename T>
+void Vector<T>::insertSort(int lo, int hi)
+{
+	if (hi-lo<2)
+		return;
+	for (int i = lo+1;i < hi;i++)
+	{
+		T temp =_elem[i];
+		int j=0;
+		for (j = i-1;j >=lo&&_elem[j]>temp;j--)
+		_elem[j+1] = _elem[j];
+		_elem[j + 1] = temp;
+	}
+}
+template<typename T>
+inline void Vector<T>::bubbleSort(int lo, int hi)
+{
+	for (int j = lo;j < hi;j++)
+	{
+		for (int i = lo;i < hi-j-1;i++)
+		{
+			if (_elem[i] > _elem[i + 1])
+			{
+				swap(_elem[i], _elem[i + 1]);
+			}
+		}
+	}
+}
+template<typename T>
+inline void Vector<T>::mergeSort(int lo, int hi)
+{
+	if (hi - lo < 2) return;
+	int middle = (lo + hi) / 2;
+	mergeSort(lo, middle);
+	mergeSort(middle, hi);
+
+	//merge
+    // 备份左右半区
+	int leftLen = middle - lo;
+	int rightLen = hi - middle;
+	T* temp1 = new T[leftLen];
+	T* temp2 = new T[rightLen];
+	for (int i = 0; i < leftLen; i++) temp1[i] = _elem[lo + i];
+	for (int j = 0; j < rightLen; j++) temp2[j] = _elem[middle + j];
+
+	// 合并逻辑
+	int p = 0, q = 0, idx = lo;
+	while (p < leftLen && q < rightLen) 
+	{
+		_elem[idx++] = (temp1[p] <= temp2[q]) ? temp1[p++] : temp2[q++];
+	}
+	while (p < leftLen) _elem[idx++] = temp1[p++];
+	while (q < rightLen) _elem[idx++] = temp2[q++];
+
+	delete[] temp1;
+	delete[] temp2;
+}
+template<typename T>
+inline void Vector<T>::quickSort(int lo, int hi)
+{
+	if (hi - lo < 2)return;
+	//选择基准
+	int sta = _elem[lo];
+	T *temp = new T[hi - lo];
+	int begin = 0, end = hi - lo;
+	for (int i = lo+1;i < hi;i++)
+	{
+		if (_elem[i] > sta)
+		{
+			temp[--end] = _elem[i];
+		}
+		else
+		{
+			temp[begin++] = _elem[i];
+		}
+
+	}
+	temp[begin] = sta;
+	for (int k = 0;k < hi-lo;k++)
+	{
+		_elem[lo+k] = temp[k];
+	}
+	quickSort(lo, lo+begin);
+	quickSort(lo+begin+1, hi);
+	
+	delete[] temp;
+}
+// 58,56,23,7,1,5,9,3,87,60
+// 
+
+template<typename T>
+void Vector<T>::sort(int lo,int hi)
+{
+	quickSort(lo, hi);
+}
+
+template<typename T>
+inline Vector<T>::Vector(T const* A, int n)
+{
+	copyFrom(A, 0, n);
+}
--- DataStructure/main.cpp
+++ DataStructure/main.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
+#include"Vector.h"
 
 int main() {	
-	std::cout << "Hello World" << std::endl;
+	int a[] = { 58,56,23,7,1,5,9,3,87,60 };
+	Vector<int> t(a,10);
+	t.print();
+	t.sort(0,10);
+	t.print();
+
 }
